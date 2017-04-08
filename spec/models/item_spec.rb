30c1efa13c8b9d552386bd12e9a15a64121de2e8
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:36:33 +0300

    [ITEMs] added model spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect belong_to(:todo) }
+  it { expect validate_presence_of(:name) }
 end
