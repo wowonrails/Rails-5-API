498e7e2585d4f699a32b9e6862722dc26591e38a
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:36:24 +0300

    [TODOs] added model spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect have_many(:items).dependent(:destroy) }
+  it { expect validate_presence_of(:title) }
+  it { expect validate_presence_of(:created_by) }
 end
