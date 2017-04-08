f5d4e634dac2528462522f95c6e236237114e0fe
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:33:27 +0300

    [TODOS] added base sceleton

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408152720) do
+ActiveRecord::Schema.define(version: 20170408153309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
