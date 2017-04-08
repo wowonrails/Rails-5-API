17a4f0bf53b4691df910f4f9f27652996cb4a2fe
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:33:57 +0300

    [TODO items] base sceleton

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408153309) do
+ActiveRecord::Schema.define(version: 20170408153339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "done"
+    t.integer  "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_items_on_todo_id", using: :btree
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170408153309) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "todos"
 end
