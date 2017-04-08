0c457f109f1d0282a1e7fd2e5f8df91de0778250
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:56:39 +0300

    [API] Authentication: added Expiration of Tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,31 @@
 class User < ApplicationRecord
   has_secure_password
-  has_secure_token :auth_token
-
-  # This method is not available in has_secure_token
-  def invalidate_token
-    self.update_columns(token: nil)
-  end
+  has_secure_token
 
   def self.valid_login?(email, password)
     user = find_by(email: email)
     if user && user.authenticate(password)
       user
     end
+  end
+
+  def allow_token_to_be_used_only_once
+    regenerate_token
+    touch(:token_created_at)
+  end
+
+  def logout
+    invalidate_token
+  end
+
+  def with_unexpired_token(token, period)
+    where(token: token).where('token_created_at >= ?', period).first
+  end
+
+  private
+
+  def invalidate_token
+    update_columns(token: nil)
+    touch(:token_created_at)
   end
 end
