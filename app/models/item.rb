3bca67ea9e6378f1db7057ae5f6c4f53cee11a61
Vladimir Marukhin <wowaonrails@gmail.com>
2017-04-08 18:38:08 +0300

    [TODOs and ITEMs] added associations and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :todo
+
+  validates :name, presence: true
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+  has_many :items, dependent: :destroy
+
+  validates :title, :created_by, presence: true
 end
